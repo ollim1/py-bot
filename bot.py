961f72ce108fbb6c6a0413468117a8c320d4501f
Olli Määttä <ollimaatta@gmail.com>
2022-02-23 00:08:05 +0200

    add cli arguments -l, -n

--- bot.py
+++ bot.py
@@ -1,23 +1,43 @@
 import os
 import time
+import sys
+import argparse
 import multiprocessing as mp
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
-
 dirname = os.path.dirname(__file__)
 driverfile = os.path.join(dirname, "ext/chromedriver")
 
-def worker(instance):
-    print("starting instance " + str(instance))
+def worker(tup):
+    print("starting instance " + str(tup[1]))
     executable_path = driverfile
     options = Options()
     options.add_experimental_option("detach", True)
     driver = webdriver.Chrome(chrome_options=options, executable_path=driverfile)
-    driver.get("https://www.amd.com/en/direct-buy/fi")
+    driver.get(tup[0])
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Open url in n windows")
+    parser.add_argument("-l")
+    parser.add_argument("-n")
     instances = 4
-    url = "http://www.amd.com/"
-    p = mp.Pool(instances)
-    p.map(worker, range(instances))
+    args = parser.parse_args()
+
+    if args.l is None:
+        printf("no location specified")
+        sys.exit()
+    else:
+        url = args.l
+    if args.n is None:
+        instances = 4
+    else:
+        instances = int(args.n)
+
+    t = []
+    for i in range(instances):
+        t.append((url, i))
+    p = mp.Pool()
+    p.map(worker, t)
+    p.close()
+    p.join()
